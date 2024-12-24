5c4a298e93953416d89f84ada209301bada0b8e9
Allan <allan.mendes0976@gmail.com>
2024-12-23 23:26:29 -0300

    civilization.json update

--- src/models/civilizations-model.ts
+++ src/models/civilizations-model.ts
@@ -1,34 +1,7 @@
-export interface civilizationsmodel {
+export interface CivilizationModel {
   id: number;
   civilizacao: string;
-  planeta: {
-    nome: string;
-    sistemasolar: {
-      estrelas: string[];
-      classificacaoestrela: string;
-    };
-    distancia: {
-      terra: string;
-      vulcano: string;
-    };
-    populacao: {
-      total: string;
-      picohistorico: string;
-      taxanatalidade: string;
-    };
-  };
-  fisiologia: {
-    aparencia: {
-      pele: string;
-      cabelo: string;
-      alturamedia: string;
-      antenas: string;
-    };
-    sentidos: {
-      visao: string;
-      olfato: string;
-      antenas: string;
-    };
-  };
+  planeta: string;
+  fisiologia: string;
   historia: string;
 }
